32fdc18c5eac6bdee6afde885d928582ca6f1e42
Steve Pothier <pothiers@gmail.com>
2020-04-02 18:52:37 -0700

    snap

--- pyphi/data_models.py
+++ pyphi/data_models.py
@@ -1,17 +1,33 @@
-# see
+# see:
 #  https://github.com/wmayner/pyphi/blob/feature/implicit-tpm/pyphi/_tpm.py
-#    ~/Downloads/_tpm.py
+#    e.g. ~/Downloads/_tpm.py
 #  tpm.py
+
+# TODO:
+#  vitual matrix via func(state) -> state
+#  prev, next to iterate through square TPM
+#  Example code that runs with network of size: 10^1,10^2,10^3,10^4 (...)
+#    High state count.  Long state-vector as DF index.
+#    Use generators instead of lists.
+#  Non-square CM matrix
+#  Doc functional requirements of system (as built vs future)
+#  States as HEX; for up to 16 states in compact idx easily converted to decimal
+#  DF index by state-vector (state for each node) could use MultiIndex EXCEPT
+#    this would not be scalable to high number of nodes.
+
 # Python internals
 import itertools
 from random import random
 from collections.abc import Sequence
 from functools import reduce
 import operator
+import subprocess
 # External packages
 import networkx as nx
+from networkx.drawing.nx_pydot import write_dot
 import pandas as pd
 import numpy as np
+from numpy.random import choice as pchoice
 # Local packages
 import pyphi
 from pyphi.examples import basic_noisy_selfloop_network
@@ -33,6 +49,14 @@ from pyphi.convert import sbs2sbn, sbn2sbs
 # Real systems may have continuous Prob yielding nearly fully connected matrix.
 #
 # Supporting heterogenous states per node may break things.
+#
+# For network of 4 nodes, the SBN of TPM (multi-dim form) is as:
+#   first 4 dims are state of Node(1,2,3,4) at t
+#   last dim is prob of Node(1,2,3,4) being ON at t+1
+#   ESSENTIALY: 2d[state, node] => prob of node being ON
+#   This can only support binary nodes because prob is for single state 
+#   (other binary state = 1-prob)
+
 
 
 def prod(iterable):
@@ -65,6 +89,7 @@ def example_causation_tpm():
                       [0.51548114, 0.89159624]])
     df = pd.DataFrame(data=probs, index=CBA_states, columns=D_states)
     return df
+
 
 class NU_Node():  # NU:: Not Used!!!
     """Node in network. Supports more than just two states but downstream 
@@ -105,13 +130,17 @@ def gen_node_lut(nodes):
     return lut, labels, num_combo_states
 
 def all_state_combos(node_lut):
-    return itertools.product(*[range(cnt) for (lab,cnt) in node_lut.values()])
+    """Return generator for states as HEX."""
+    # node_lut[index] => (label,state_cnt) e.g. {0: ('A', 2),  1: ('C', 3)}
+    states = itertools.product(*[range(cnt) for (lab,cnt) in node_lut.values()])
+    hstates = (''.join(hex(s)[2:] for s in sv) for sv in states)
+    return hstates
 
 # lut, labs, combos = gen_node_lut(['A', 2, ('C', 3), [4, 6]])
 # all_state_combos(lut) => [...,(1, 1, 0, 5), (1, 1, 1, 0), (1, 1, 1, 1),...]
 
 
-class CM():
+class CM():  # @@@ Allow this to be non-square (e.g. feed-forward)
     """Connectivity Matrix
 
     cm is Adjacency matrix as python list of lists """
@@ -125,16 +154,15 @@ class CM():
                have 2 elements  (<node_label>, <number_of_states_of_node>)
         """        
 
-        if cm is not None:
-            node_cnt = len(cm)
-            nodes = list(range(node_cnt))
-        elif node_labels is not None:
-            node_cnt = len(node_labels)
-            nodes = node_labels
-        else: # default to two nodes
-            node_cnt = 2
-            nodes = list(range(node_cnt))
-        lut, labels,combos = gen_node_lut(nodes)
+        if cm is None:
+            cm = np.zeros((2,2))
+        node_cnt = len(cm)
+        labels = list(range(node_cnt))
+        if node_labels is not None:
+            assert node_cnt == len(node_labels), (
+                f'Number of labels {len(node_labels)} must be same as '
+                f'len(cm) {node_cnt}')
+            labels = node_labels
         self._node_labels = labels
         self._df = pd.DataFrame(data=cm, index=labels, columns=labels)
 
@@ -169,12 +197,13 @@ class CM():
         return self
         
 
-            
-class TransProb():
-    """TRANSition PROBability matrix (and other formats)"""
 
     
-    def __init__(self, in_nodes, out_nodes, probabilities, defaultspn=2):
+class TransProb(): # @@@ This could be "virtual". Func instead of matrix.
+    """TRANSition PROBability matrix (and other formats)"""
+    
+    def __init__(self, in_nodes=None, out_nodes=None, probabilities=None,
+                 defaultspn=2):
         """Create and store a Transition Probability Matrix
 
         This is a directed adjacency matrix from the states of in_nodes 
@@ -182,37 +211,42 @@ class TransProb():
         (for all nodes) is given by spn. Thus the number of rows is:
         spn^|in_nodes|.  The number of columns is: spn^|out_nodes|. 
         The probabilities parameter is an 2D array that must this shape.
+        If not given, probabilities defaults to zeros of correct shape.
 
         in_nodes, out_nodes: list of nodes where a node can be an int, string, 
         tuple, or list.  If tuple or list it must have 2 elements 
         (<node_label>, <number_of_states_of_node>)
         """
+        if in_nodes is None:
+            in_nodes = range(2)
+        if out_nodes is None:
+            out_nodes = range(2)
+        (self.in_node_lut,
+         self.in_node_labels,
+         self.num_in_states) = gen_node_lut(in_nodes)
 
-        lut, labels,combos = gen_node_lut(in_nodes)
-        self.in_node_lut = lut
-        self.in_node_labels = labels
-        self.num_in_states = combos
-        assert combos == probabilities.shape[0], (
+        (self.out_node_lut,
+         self.out_node_labels,
+         self.num_out_states) = gen_node_lut(out_nodes)
+
+        if probabilities is None:
+            probabilities = np.zeros((self.num_in_states, self.num_out_states))
+        assert self.num_in_states == probabilities.shape[0], (
             f'dim0[{probabilities.shape[0]}] of probabilities '
-            f'must equal {combos} '
+            f'must equal {self.num_in_states} '
             '(prod of state count for all in_nodes)')
-
-
-        lut, labels, combos = gen_node_lut(out_nodes)
-        self.out_node_lut = lut
-        self.out_node_labels = labels
-        self.num_out_states = combos
-        assert combos == probabilities.shape[1], (
+        assert self.num_out_states == probabilities.shape[1], (
             f'dim1[{probabilities.shape[1]}] of probabilities '
-            f'must equal {combos} '
+            f'must equal {self.num_out_states} '
             '(prod of state count for all out_nodes)')
 
-        in_states = all_state_combos(self.in_node_lut)
-        out_states = all_state_combos(self.out_node_lut)
-
+        self.istates = list(all_state_combos(self.in_node_lut))
+        self.ostates = list(all_state_combos(self.out_node_lut))
+        self._current = self.istates[0] # default start state
+        
         self._df = pd.DataFrame(data=probabilities,
-                                index=in_states,
-                                columns=out_states)
+                                index=self.istates,
+                                columns=self.ostates)
         
     def __len__(self):
         return len(self.in_node_labels) * len(self.out_node_labels)
@@ -224,12 +258,44 @@ class TransProb():
                 f'out_labels: {self.out_node_labels}')
 
     @property
+    def state(self):
+        return self._current
+
+    def set_state(self, state):
+        self._current = state
+
+    def _state_probs(self, state):
+        # Allow probabilities to not sum to one. Better to fix on input! @@@
+        def norm(raw):
+            return [float(i)/sum(raw) for i in raw]
+        return norm(self._df[state])
+        
+    def next_state(self, current=None):
+        """Return a valid state at t+1 given current state at t.
+        The next state is chosen from list of possibles to fit the distribution
+        of out states.
+        """
+        state = self._current if current is None else current
+        probs = self._state_probs(state)
+        nextstate = pchoice(list(self._df.columns), 1, p=probs)[0]
+        self._current = nextstate
+        return nextstate
+
+    @property
     def in_nodes(self):
         return self.in_node_labels
 
     @property
+    def in_states(self):
+        return list(self._df.index)
+
+    @property
     def out_nodes(self):
         return self.out_node_labels
+    
+    @property
+    def out_states(self):
+        return list(self._df.columns)
 
     @property
     def df(self):
@@ -242,34 +308,64 @@ class TransProb():
         return self._df.to_numpy()
 
     @property
-    def sbn(self):
+    def legacy_sbn(self):
         """State-By-Node form. 
         """
         return sbs2sbn(self._df.to_numpy())
+
+    # For network of 4 nodes, the SBN of TPM (multi-dim form) is as:
+    #   first 4 dims are state of Node(1,2,3,4) at t
+    #   last dim is prob of Node(1,2,3,4) being ON at t+1
+    #   ESSENTIALY: 2d[state, node] => prob of node being ON
+    #   This can only support binary nodes because prob is for single state 
+    #   (other binary state = 1-prob)
+    @property
+    def sbn(self):
+        """State-By-Node form. 
+        """
+        nodes = self.in_node_labels + self.in_node_labels
+        (lut, labels, num_states) = gen_node_lut(nodes)
+        # Make sure all nodes are binary
+        assert all([(cnt == 2) for (lab,cnt) in lut.values()])
+
+        states = all_state_combos(lut)
+        # see convert.py "little-endian"
+        revstates = (reversed(s) for s in states) # generator
   
-    def graph(self):
+    def graph(self, min_prob=0.02, pngfile=None):
         """Return networkx DiGraph with edge.weight=probability.
         """
-        return nx.DiGraph(_tpm)
+        #!G = nx.DiGraph(self._df)
+        df = self._df
+        dod = dict()
+        for i in self._df.index:
+            d = dict()
+            for j in df.columns:
+                prob = df[i][j]
+                if prob >= min_prob:
+                    d[j] = {'label': f'{int(100*round(prob,2))}'}
+            dod[i] = d
+        G = nx.DiGraph(dod)
+                
+        if pngfile is not None:
+            dotfile = pngfile + ".dot"
+            write_dot(G, dotfile)
+            cmd = (f'dot -Tpng -o{pngfile} {dotfile} ')
+            with open(pngfile,'w') as f:
+                subprocess.check_output(cmd, shell=True)
+        return G
 
     @property
     def to_legacy(self):
         """Pure numpy representation which ignores node labels."""
         return self._df.to_numpy()
 
-    def from_legacy(self, tpm, labels=None):
-        if labels is None:
-            labels = list(range(len(tpm)))
-        lut, labels,combos = gen_node_lut(labels)
-        states = all_state_combos(lut)
-
-        self.in_node_lut = lut.copy()
-        self.out_node_lut = lut.copy()        
-
-        self.in_node_labels = labels
-        self.out_node_labels = labels
-
-        self._df = pd.DataFrame(data=tpm, index=states, columns=states)
+    def from_legacy(self, legacy_tpm, labels=None):
+        """Overwrite content of self to make this TPM contain legacy tpm.
+        """
+        self. __init__(in_nodes=labels, out_nodes=labels,
+                       probabilities=legacy_tpm,
+                       defaultspn=2)
         return self
         
 
